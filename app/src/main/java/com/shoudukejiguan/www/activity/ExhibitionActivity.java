e5a4f308122256957c3cbbcbf1794e428e7d672a
dmz1024 <894350911@qq.com>
2016-07-14 16:50:21 +0800

    搭建完成-票务预订-增加了时间筛选

---
+++ app/src/main/java/com/shoudukejiguan/www/activity/ExhibitionActivity.java
@@ -0,0 +1,29 @@
+package com.shoudukejiguan.www.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.shoudukejiguan.www.R;
+
+public class ExhibitionActivity extends BaseActivity {
+
+    @Override
+    protected int getRid() {
+        return R.layout.activity_exhibition;
+    }
+
+    @Override
+    protected void initTitleBar() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
